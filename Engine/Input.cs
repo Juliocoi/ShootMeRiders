fa00969379918c2ba570dccec027716160ac34d8
Julio Cesar Amorim <amorim.jc@gmail.com>
2024-04-19 18:32:06 -0300

    Cria classe Engine/Input

---
+++ Engine/Input.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework.Input;
+
+namespace ShootMeRiders.Engine;
+
+public static class Input
+{
+	private static List<Keys> _consumedKeys = new List<Keys>();
+
+	public static bool GetKey(Keys key)
+	{
+		return Keyboard.GetState().IsKeyDown(key);
+	}
+
+	public static bool GetKeyDown(Keys key)
+	{
+		bool result = false;
+
+		if(!_consumedKeys.Contains(key))
+		{
+			if(GetKey(key))
+			{
+				_consumedKeys.Add(key);
+				result = true;
+			}
+		}
+
+		return result;
+	}
+
+	public static void Update()
+	{
+		if(_consumedKeys.Count > 0)
+		{
+			KeyboardState keyboardState = Keyboard.GetState();
+
+			for (int i = _consumedKeys.Count - 1; i >= 0; i--)
+			{
+				Keys key = _consumedKeys[i];
+				if(keyboardState.IsKeyUp(key))
+				{
+					_consumedKeys.RemoveAt(i);
+				}
+			}
+		}
+	}
+
+}
+
